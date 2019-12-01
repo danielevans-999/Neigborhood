8fa0688e2dae7948111da523d0418473f9cd6b98
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-01 11:15:05 +0300

    create the models.

--- neigborhood_app/models.py
+++ neigborhood_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neigborhood(models.Model):
+    pass
+    
+class UserProfile(models.Model):
+    pass
+    
+class Business(models.Model):
+    pass
+    
+class Post(models.Model):
+    pass
