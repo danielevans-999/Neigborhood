bcdab877cfd54051e4728c71a3691c78e8f012bb
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-01 09:50:02 +0300

    Adds our application to the installed apps

--- neigborhood_project/settings.py
+++ neigborhood_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'neigborhood_app.app.NeigborhoodAppConfig',
+    'neigborhood_app.apps.NeigborhoodAppConfig',
 ]
 
 MIDDLEWARE = [
