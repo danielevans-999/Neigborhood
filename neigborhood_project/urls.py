6a325c74c9ceaba11ea7325df67f5d35a1ac4db9
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-02 20:52:43 +0300

    Adds a new path

--- neigborhood_project/urls.py
+++ neigborhood_project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('neigborhood_app.urls'))
-    # path('logout/', views.logout,{"next_page":'/'}),
+    path('', include('neigborhood_app.urls')),
+    path('logout/' ,views.LogoutView.as_view(), {'next':'/'}),
 ]
