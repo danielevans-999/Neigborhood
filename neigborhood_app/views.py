15c21a740faf16ebc260811572ea52d937d035d6
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-02 15:46:43 +0300

    Adds the single_hood view function

--- neigborhood_app/views.py
+++ neigborhood_app/views.py
@@ -5,3 +5,7 @@ from . models import *
 def home(request):
     neigborhoods = Neigborhood.objects.all()
     return render(request,'neigborhood/index.html',{'neigborhoods':neigborhoods})
+
+def single_neigborhood(request,id):
+    single_hood = Neigborhood.objects.get(pk=id)
+    return render (request, 'neigborhood/neigborhood.html',{'hood':single_hood})
