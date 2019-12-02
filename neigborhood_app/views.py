7bbf91694051de1a2ff87cf9941c23dd7690d109
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-02 17:50:06 +0300

    Adds two view functions the profile and update_profile view function

--- neigborhood_app/views.py
+++ neigborhood_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import *
-
+from . forms import UpdateProfileForm
 
 def home(request):
     neigborhoods = Neigborhood.objects.all()
@@ -8,4 +8,27 @@ def home(request):
 
 def single_neigborhood(request,id):
     single_hood = Neigborhood.objects.get(pk=id)
-    return render (request, 'neigborhood/neigborhood.html',{'hood':single_hood})
+    business = single_hood.business_set.all
+    post = single_hood.post_set.all
+    return render (request, 'neigborhood/neigborhood.html',{'hood':single_hood,'bizz':business,'posts':post})
+
+def profile_info(request):
+    
+    current_user=request.user
+    profile_info = UserProfile.objects.filter(user=current_user).first()
+    post =  request.user.post_set.all()
+    
+    return render(request,'neigborhood/profile.html',{'posts':post,'profile':profile_info,'current_user':current_user})
+
+def update_profile(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    # form = UpdateProfileForm(instance=request.user)
+    
+    if request.method == "POST":
+        form =  UpdateProfileForm(request.POST,request.FILES,instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = UpdateProfileForm(instance=request.user.userprofile)
+        return render(request,'neigborhood/update_profile.html', {'form':form})
