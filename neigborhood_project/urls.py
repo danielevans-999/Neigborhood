6bc0d09f8f02454230d5c47edfa053aa82cbdeae
danielevans-999 <danielevans.karani@gmail.comgit>
2019-12-02 12:35:07 +0300

    Adds the authentication backend

--- neigborhood_project/urls.py
+++ neigborhood_project/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('neigborhood_app.urls'))
+    # path('logout/', views.logout,{"next_page":'/'}),
 ]
